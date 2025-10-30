a4cca26ca9baa7fbe33cf02f904a993650b309e4
Fernando Magon <fernando.mw.66@gmail.com>
2025-10-29 21:34:01 -0300

    leitor de cep atualizado

--- todas_atividades/app/cep.js
+++ todas_atividades/app/cep.js
@@ -1,5 +1,5 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Button, StyleSheet, Text, TextInput, Image, Keyboard } from "react-native";
+import { SafeAreaView} from "react-native-safe-area-context";
+import {  View, Button, StyleSheet, Text, TextInput, Image, Keyboard } from "react-native";
 import { useState, useEffect } from "react";
 
 async function getCep(cepBusca) {
@@ -26,24 +26,34 @@ export default function Cep() {
         setCidade(cepTemp.city)
         setBairro(cepTemp.neighborhood || null)
         setRua(cepTemp.street || null)
-        
     }
 
     return (
         <SafeAreaView style={estilos.container}>
+            <View style={estilos.caixaTitulo}>
+                <Text style={estilos.titulo}>Escreva um CEP na caixa abaixo</Text>
+            </View>
             <TextInput
                 value={cepPesquisar}
                 onChangeText={setCepPesquisar}
                 style={estilos.caixaInput}
+                keyboardType="numeric"
+                maxLength={8}
+                placeholder="Digite o CEP (ex: 12345678)"
             />
 
-            <Button title="Pesquisar" onPress={carregarCep} />
+            <Button 
+                title="Pesquisar" 
+                onPress={carregarCep}
+            />
 
-            <Text>CEP: {cep}</Text>
-            <Text>Estado: {estado}</Text>
-            <Text>Cidade: {cidade}</Text>
-            <Text>Bairro: {bairro}</Text>
-            <Text>Rua: {rua}</Text>
+            <View style={estilos.caixaResposta}>
+                <Text style={estilos.resposta}>CEP: {cep}</Text>
+                <Text style={estilos.resposta}>Estado: {estado}</Text>
+                <Text style={estilos.resposta}>Cidade: {cidade}</Text>
+                <Text style={estilos.resposta}>Bairro: {bairro}</Text>
+                <Text style={estilos.resposta}>Rua: {rua}</Text>
+            </View>
 
         </SafeAreaView>
     )
@@ -56,15 +66,38 @@ const estilos = StyleSheet.create({
         flex: 1
     },
 
+    caixaTitulo: {
+        alignItems: 'center',
+        marginVertical: 10,
+    },      
+    
+    caixaResposta: {
+        height: 150,
+        justifyContent: 'space-around'
+    },
+
+    resposta: {
+        borderColor: '#000',
+        borderBottomWidth: 1,
+        borderRadius: 4,
+        paddingLeft: 5,
+    },
+    
+    titulo: {
+        fontSize: 22,
+        fontWeight: 'bold'
+    },      
+
     caixaInput: {
         backgroundColor: '#fff',
-        width: '90%',
+        width: '100%',
         height: 35,
         borderColor: '#000',
         borderWidth: 1,
         borderRadius: 4,
         paddingLeft: 5,
         marginVertical: 8,
+
     },
 
 })
